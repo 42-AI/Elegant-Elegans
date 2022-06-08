66365f312885f07439bce7a7576e3b847a13e467
COTHSC <thierryscully.colomban@gmail.com>
2022-06-08 19:07:06 +0200

    renamed file, minor cleanup, more descriptive error messages

--- json_metadata_parser/json_metadata_parser.py
+++ json_metadata_parser/json_metadata_parser.py
@@ -1,18 +1,9 @@
-from curses import meta
 from datetime import datetime
-import json
 from matplotlib.font_manager import json_load
-import os
-import re
-from PIL import Image
-from PIL.TiffTags import TAGS
-from numpy import iinfo
 import cv2 as cv
 
-
+#Some global vars to be replaced by out from args parser
 target_directory = "../data"
-
-
 interval = "Interval_ms"
 summary = "Summary"
 
@@ -44,9 +35,6 @@ def loadTif(tifPath):
     cv_img = cv.imread(tifPath)
     return cv_img
 
-
-
-
 def checkImages(metadata, directoryPath):
     total_time_ms = 0
     frames = 0
@@ -57,39 +45,35 @@ def checkImages(metadata, directoryPath):
         if obj.startswith("Metadata-Default"):
             filename = obj.rsplit('/', 1)[-1]
             if filename != summary:
-                #TODO here it should throw an error if it does not find the tif file
-                # cv_img = cv.imread(directoryPath + "/" + filename)
 
                 cv_img = loadTif(directoryPath + "/" + filename)
                 if cv_img is None:
                     print("could not load: ", directoryPath + "/" + filename, " aborting")
                     return
-                actual_height, actual_width, layers = cv_img.shape
-                # check if the size of the image matches the expected size from metadata file
-                expected_width = metadata[obj]["Width"]
-                expected_height = metadata[obj]["Height"]\
 
+                actual_height, actual_width, layers = cv_img.shape
+                expected_width, expected_height = metadata[obj]["Width"], metadata[obj]["Height"]
                 if actual_height != expected_height or actual_width != expected_width:
-                    print(".tif metadata mismatch")
+                    print(".tif metadata mismatch on frame: ", directoryPath + "/" + filename, " aborting")
                     return
-                # TODO throw error if sizes don't match
-                # checks for missing frames
+                
                 currentFrame = metadata[obj]["Frame"]
                 if currentFrame == 0:
                     time_to_first_image = metadata[obj]["ElapsedTime-ms"]
                     time_since_last_frame = 0
                 else:
                     time_since_last_frame
+
+                # checks for missing frames
                 if currentFrame != expect_frame_no:
-                    print("THERE WAS AN ERROR")
+                    print("expected frame no: ", expect_frame_no, " but got frame no: ", currentFrame, " check for missing frames")
                     break
                 tmp = total_time_ms
                 total_time_ms = metadata[obj]["ElapsedTime-ms"] - time_to_first_image
                 time_since_last_frame = total_time_ms - tmp
                 expect_frame_no = expect_frame_no + 1
                 frames = frames + 1
-            # print(time_since_last_frame)
-            # print(meta_dict) #there may be some more info we want to check in img metadata
+
     if frames == expected_frames:
         print("all expected frames were found")
         print("total capture time: ", total_time_ms)
