81b5773d136c10aeadafa8b36773a575dadf56a5
Jee Park <79132132+jhparkkkk@users.noreply.github.com>
2022-06-20 21:48:50 +0200

    parse_ok (#28)
    * parse_ok
    
    * function parser done, unittest in progress
    
    * parser done and completed with -o and --output
    
    * unit test done
    
    * unit test done
    
    * parse_ok
    
    * function parser done, unittest in progress
    
    * parser done and completed with -o and --output
    
    * unit test done
    
    * unit test done
    
    * .vscode file deleted
    
    * good practice (feature converter img to video): moving tests associated to parsing into the right folder (tests)
    
    * good practice (feature converter img to video): delete of unwanted files
    
    * pull request resolution : help to -output argument detailed and  [description] and .tiff file removed
    
    * fix (test): add of parent directory in a test file to have acces to parent module (in this case converter)
    
    * fix and form (converter): moving some functions in parser.py and other in checker.py. Removing the function from __main__ and defining main() function calling parser and checker
    
    Co-authored-by: Shellen Dorlin <shdorlin@student.42.fr>
    Co-authored-by: Jee <jeehyun24park@gmail.com>
    Co-authored-by: madvid <mdavid@student.42.fr>

--- converter/__main__.py
+++ converter/__main__.py
@@ -1,84 +1,18 @@
-import argparse
-
-# ########################################################################## #
-#                                CONSTANTS                                   #
-# ########################################################################## #
+from converter.checker import path_checker, path_inside_checker
+from converter.parser import parser
 
 
 # ########################################################################## #
 #                                FUNCTIONS                                   #
 # ########################################################################## #
 
-def parser() -> dict:
-    """ [Description]
-    Return:
-    -------
-    """
-    # use argparse.ArgumentParser()
-    # then add_argument method
-    # see https://docs.python.org/3/library/argparse.html
-    return parser.parse_args()
 
-
-def path_checker(path: str):
-    """ [Description]
-
-    Arguments:
-    ----------
-        path (str): path to the directory
-    Raise:
-    ------
-        Depends on the kind of issue encountered:
-        * NotADirectoryError if ...
-        * PermissionError if ...
-    """
-    pass
-
-
-def path_inside_checker(dir_path):
-    """ [Description]
-    
-    Arguments:
-    ----------
-        ...
-    Raises:
-        ...
-    """
-    pass
-
-
-def json_parser(dir_path):
-    """ [Description]
-    
-    Arguments:
-    ----------
-        ...
-    Raises:
-        ...
-    """
-    pass
-
-
-def tiff_files_checker(metadata):
-    """ [Description]
-    
-    Arguments:
-    ----------
-        ...
-    Raises:
-        ...
-    """
-    pass
-
-
-# ########################################################################## #
-#                                   MAIN                                     #
-# ########################################################################## #
-
-if __name__ == '__main__':
+def main():
     # parsing the argument(s)
     args = parser()
-    dir_path = args['path']
+
+    # print(args)
+    dir_path = args.path
 
     # checker of the path
     path_checker(dir_path)
@@ -86,9 +20,16 @@ if __name__ == '__main__':
     # checker of the inside of the path
     path_inside_checker(dir_path)
 
-    # Parsing the json metadata file
-    metadata = json_parser(dir_path)
+    # # Parsing the json metadata file
+    # metadata = json_parser(dir_path)
 
-    # checker of the tiff images based on metadata
-    tiff_files_checker(metadata)
-    
+    # # checker of the tiff images based on metadata
+    # tiff_files_checker(metadata)
+
+
+# ########################################################################## #
+#                                   MAIN                                     #
+# ########################################################################## #
+
+if __name__ == "__main__":
+    main()
---
+++ converter/checker.py
@@ -0,0 +1,50 @@
+import os
+
+NB_LIMIT = 100
+
+# Checker related to the argument parsed.
+def path_checker(path: str):
+    """ Check the existence and access of a directory
+
+    Arguments:
+        path (str): path to the directory
+
+    Raise:
+        Depends on the kind of issue encountered:
+        * NotADirectoryError if the directory doesn't exist or is not a directory
+        * PermissionError if the user doesn't h—ave access to the directory
+    """
+    if not os.path.isdir(path):
+        raise NotADirectoryError(path + " is not a directory.")
+    if not os.access(path, os.R_OK | os.W_OK):
+        raise PermissionError("Permission denied to " + path)
+
+
+def path_inside_checker(dir_path: str):
+    """ Check that the files inside the directory are .tiff or .json,
+        that a .json file exists and that there are at least 100 .tiff files
+
+    Arguments:
+        dir_path : path to directory
+
+    Raises:
+        Depends on the kind of issue encountered:
+        * If there is no .json file
+        * If there are fewer than 100 .tiff files
+        * If there is a file other than .tiff or .json
+    """
+    num_tiff = 0
+    num_json = 0
+    for file in os.listdir(dir_path):
+        if file[-5:] == ".tiff":
+            num_tiff += 1
+        elif file[-5:] == ".json":
+            num_json += 1
+        else:
+            raise Exception("File other than .tiff or .json found")
+    if num_json == 0:
+        raise Exception("No .json file found")
+    if num_json > 1:
+        raise Exception("More than one .json file found")
+    if num_tiff < NB_LIMIT:
+        raise Exception("Number of .tiff files insufficient (min required 100)")
