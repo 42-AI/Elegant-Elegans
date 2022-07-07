7d63d237b8a24b4ae7e2af0d9b8c9fc4d562f23c
Roberto Catini <roberto.catini@gmail.com>
2022-07-07 16:59:26 +0200

    added documentation

--- converter/json_parser.py
+++ converter/json_parser.py
@@ -7,6 +7,17 @@ interval = "Interval_ms"
 summary = "Summary"
 
 def loadMetadata(directoryPath):
+    """Loads the metadata file in a python dictionary.
+
+    Args:
+        directoryPath (string): directory path to json file to load
+
+    Raises:
+        Exception: raise an error if an unknown format is passed
+
+    Returns:
+        dict: the loaded metadata 
+    """
     try:
         file_path = directoryPath + "/metadata.txt"
         metadata = json.load(open(file_path))
@@ -15,10 +26,29 @@ def loadMetadata(directoryPath):
         raise Exception("Error trying to load metadata.txt")
 
 def padMicroSecs(timestamp):
+    """Pads the microsecond field with zeros on the left
+
+    Args:
+        timestamp: the timestamp that results from the json file
+
+    Returns:
+        str: the metadata encoded in a python dictionary
+    """
     parts = timestamp.split('.')
     return '.'.join(parts[:-1] + ['{:06d}'.format(int(parts[-1]))])
 
 def getStartTime(metadata):
+    """Get the start time of the video
+
+    Args:
+        metadata: the metadata resulting from loadMetadata
+
+    Raises:
+        Exception: if the start time cannot be found in the metadata
+
+    Returns:
+        datetime: the start time
+    """
     try:
         strTime = metadata[summary]["StartTime"]
         strTime_without_timezone = strTime[:-6]
@@ -30,10 +60,27 @@ def getStartTime(metadata):
         raise Exception("could not find start time in metadata file")
 
 def loadTif(tifPath):
+    """Load the image from a specified path
+
+    Args:
+        tifPath: the path to the tif file
+
+    Returns:
+        ndarray: the image
+    """
     cv_img = cv.imread(tifPath)
     return cv_img
 
 def checkImages(metadata, directoryPath):
+    """Checks if the images respect the information in the metadata
+
+    Args:
+        metadata: the metadata resulting from loadMetadata
+        directoryPath: the directory where the images are located
+
+    Returns:
+        dict: a dictionary with the validated metadata
+    """
     total_time_ms = 0
     frames = 0
     expect_frame_no = 0
