08666fc18112452ddbfb1e54cb356df260048dd7
Roberto Catini <roberto.catini@gmail.com>
2022-07-21 17:43:59 +0300

    end-to-end tests of the converter module

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,22 @@
+import os
+from os.path import exists
+import sys
+import pytest
+
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+from converter.__main__ import main
+
+@pytest.mark.parametrize(
+    "video_name, format, expected",
+    [
+        ("elegans1", "avi", "elegans1.avi"),
+        ("elegans2", "mp4", "elegans1.mp4"),
+    ],
+)
+def test_video_creation(video_name, format, expected):
+    test_dir = "test_tiffs/"
+    os.system("aws s3 sync s3://lab-nematode/tests/test_1/ " + test_dir)
+    os.system(f"python3 -m converter --path {test_dir} -o {video_name} -f {format}")
+    assert exists(expected)
